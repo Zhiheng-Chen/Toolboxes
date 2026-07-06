114d04bfb284f85cd62f528a7bc1ab89b92e4a8e
Zhiheng Chen <ericczh1@gmail.com>
2026-07-06 19:18:56 -0400

    Improve PDFTools performance; add OCR

--- JuliaTools/PDFTools.jl
+++ JuliaTools/PDFTools.jl
@@ -1,13 +1,15 @@
-# need Ghostscript and Draw.io installed
+# need MuPDF, Ghostscript, and Draw.io installed
 # make sure the path names of PDFs have ".pdf" at the end
 
 using Pkg;Pkg.activate(raw"D:\Toolboxes\JuliaTools");Pkg.instantiate()  # change to the path of the folder PDFTools.jl is in
 
+const path_Mu = raw"D:\Program Files\mupdf-1.28.0-windows\mutool.exe"  # change to local MuPDF mutool path
 const path_GS = raw"D:\Program Files\gs\gs10.06.0\bin\gswin64c.exe" # change to the local Ghostscript installation path
+const path_Tessdata = raw"D:\Program Files\gs\gs10.06.0\Resource\tessdata"  # change to Tess data path for Ghostscript OCR
 const path_Drawio = raw"D:\Program Files\draw.io\draw.io.exe"   # change to the local Draw.io installation path
 
 # repair and compress PDF
-function repairPDF(inputPath::String;outputPath::String="")
+function repairPDF(inputPath::String;outputPath::String="",method::String="MuPDF")
     if !isfile(inputPath)
         error("file not found: $inputPath")
     end
@@ -15,23 +17,32 @@ function repairPDF(inputPath::String;outputPath::String="")
         outputPath = replace(inputPath,".pdf"=>"_Fixed.pdf")
     end
 
-    # Ghostscript arguments
-    flags = ["-dSAFER",
-             "-sDEVICE=pdfwrite",
-             "-dCompatibilityLevel=1.4",
-             "-dPDFSETTINGS=/prepress",
-             "-dEmbedAllFonts=true",
-             "-dSubsetFonts=true",
-             "-dDetectDuplicateImages=true",
-             "-dCompressFonts=true",
-             "-dAutoRotatePages=/None",
-             # "-dHaveTransparency=false", # commented out to prevent black pages
-             "-o",outputPath,
-             "-f",inputPath]
+    if method == "MuPDF"
+        cmd = `"$path_Mu" clean -g "$inputPath" "$outputPath"`
+    elseif method == "Ghostscript"
+        # Ghostscript arguments
+        flags = [
+            "-dSAFER",
+            "-sDEVICE=pdfwrite",
+            "-dCompatibilityLevel=1.4",
+            "-dPDFSETTINGS=/prepress",
+            "-dEmbedAllFonts=true",
+            "-dSubsetFonts=true",
+            "-dDetectDuplicateImages=true",
+            "-dCompressFonts=true",
+            "-dAutoRotatePages=/None",
+            # "-dHaveTransparency=false", # commented out to prevent black pages
+            "-o",outputPath,
+            "-f",inputPath
+        ]
+        cmd = `$path_GS $flags`
+    else
+        error("method does not exist: $method")
+    end
 
     # execute command
     println("processing: $inputPath")
-    run(`$path_GS $flags`)
+    run(cmd)
     println("repair complete; saved to: $outputPath")
 end
 
@@ -44,20 +55,10 @@ function mergePDFs(inputFiles::Vector{String};outputPath::String="")
         outputPath = replace(inputFiles[1],".pdf"=>"_Merged.pdf")
     end
 
-    # Ghostscript arguments
-    flags = ["-dSAFER",
-             "-sDEVICE=pdfwrite",
-             "-dCompatibilityLevel=1.4",
-             "-dPDFSETTINGS=/prepress", 
-             "-dAutoRotatePages=/None",
-             "-o",outputPath]
-
-    # append input files to the flags list
-    fullCommandArgs = vcat(flags,inputFiles)
+    # MuPDF syntax: mutool merge -o output.pdf input1.pdf input2.pdf
+    fullCommandArgs = vcat(["merge","-o",outputPath],inputFiles)
     println("merging $(length(inputFiles)) files")
-    
-    # execute command
-    run(`$path_GS $fullCommandArgs`)
+    run(`"$path_Mu" $fullCommandArgs`)
     println("merge complete; saved to: $outputPath")
 end
 
@@ -73,20 +74,42 @@ function extractPages(inputPath::String,pageList::Vector{Int64};outputPath::Stri
     # convert Vector to String
     pageListString = join(pageList,",")
 
-    # Ghostscript arguments
-    flags = ["-dSAFER",
-             "-sDEVICE=pdfwrite", 
-             "-dCompatibilityLevel=1.4",
-             "-dPDFSETTINGS=/prepress", 
-             "-dAutoRotatePages=/None",
-             "-sPageList=$pageListString", 
-             "-o",outputPath,
-             "-f",inputPath]
+    # MuPDF syntax for extraction: mutool clean input.pdf output.pdf [pages]
+    cmd = `"$path_Mu" clean "$inputPath" "$outputPath" $pageListString`
+    println("extracting pages $pageListString")
+    run(cmd)
+    println("extraction complete; saved to: $outputPath")
+end
+
+# OCR 
+function OCR(inputPath::String;outputPath::String="",language::String="eng",DPI=300)
+    if !isfile(inputPath)
+        error("file not found: $inputPath")
+    end
+    if isempty(outputPath)
+        outputPath = replace(inputPath,".pdf"=>"_OCR.pdf")
+    end
+
+    # Ghostscript arguments for OCR
+    flags = [
+        "-dSAFER",
+        "-sDEVICE=pdfocr8",       # Ghostscript's OCR output device
+        "-r$(DPI)",                  # 300 DPI is the sweet spot for OCR accuracy
+        "-sOCRLanguage=$language", # default is English ("eng")
+        "-dAutoRotatePages=/None",
+        "-o",outputPath,
+        "-f",inputPath
+    ]
 
     # execute command
-    println("extracting pages $pageListString")
-    run(`$path_GS $flags`)
-    println("extraction complete; saved to: $outputPath")
+    println("running OCR via Ghostscript on: $inputPath")
+    try
+        cmd = addenv(`$path_GS $flags`,"TESSDATA_PREFIX"=>path_Tessdata)    # add a temporary environment variable
+        run(cmd)
+        println("OCR complete; saved to: $outputPath")
+    catch e
+        error("OCR failed. (Note: Ensure Ghostscript has access to the 'tessdata' folder for language packs). Error: $e")
+    end
 end
 
 # Drawio to PDF 
--- JuliaTools/README.md
+++ JuliaTools/README.md
@@ -1,6 +1,6 @@
 ## Environment Setup
-- Need [Ghostscript](https://ghostscript.com/releases/gsdnld.html) and Draw.io ([desktop](https://www.drawio.com/)) installed for PDF manipulations and conversions
-- No Python installation needed. `PythonCall.jl` and `CondaPkg.jl` will automatically build a local Python environment
+- Need [MuPDF](https://mupdf.com/releases?product=MuPDF), [Ghostscript](https://ghostscript.com/releases/gsdnld.html) and Draw.io ([desktop](https://www.drawio.com/)) installed for PDF manipulations and conversions. For Ghostscript OCR to function, need to download the [language package](https://github.com/tesseract-ocr/tessdata_best/blob/main/eng.traineddata) and put it inside the `Resource\tessdata` folder inside the Ghostscript folder.
+- No Python installation needed. `PythonCall.jl` and `CondaPkg.toml` will automatically build a local Python environment
 - To recover Julia and Python environemnts with `Project.toml`, `Manifest.toml`, and `CondaPkg.toml`, run in Julia: 
 
 ```julia
